6761a6b218abc8c26da72246ba8003c2b8eb7e51
Matheus Lima <int.matheus.lima@gmail.com>
2024-05-20 21:30:34 -0300

    add: importando rotas "HOME"  e "CLIENTE"

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
+from routes.cliente import cliente_route
 
 #inicialização
 app = Flask (__name__)
 
-#rotas
-@app.route('/')
-def ola_mundo():
-    titulo = "Gestão de Usuários"
-    usuarios = [
-        {"nome": "Guilherme", "membro_ativo": True},
-        {"nome": "João", "membro_ativo": False},
-        {"nome": "Maria", "membro_ativo": False},
-    ]
-    return render_template("index.html", titulo=titulo, usuarios=usuarios)
-
-@app.route('/sobre')
-def pagina_sobre():
-    return """
-        <b>Acecsse o meu GitHub para mais informações: </b>
-        <a href="https://github.com/Matheus-ALima">Matheus Alves Lima</a>
-    """
+app.register_blueprint(home_route)
+app.register_blueprint(cliente_route, url_prefix='/clientes')
 
 #execução
 app.run(debug=True)
