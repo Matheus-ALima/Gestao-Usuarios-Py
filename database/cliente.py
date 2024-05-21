9d92cf470f198d1ba5c194f0e443db4a666d079f
Matheus Lima <int.matheus.lima@gmail.com>
2024-05-21 16:24:09 -0300

    add: fake db - para teste

---
+++ database/cliente.py
@@ -0,0 +1,9 @@
+CLIENTES = [
+    {"id":1, "nome": "Matheus Lima", "email":"matheus@teste.com"},
+    {"id":2, "nome": "Vitória Lima", "email":"vitoria@teste.com"},
+    {"id":3, "nome": "Samuel Fernandes", "email":"samuel@teste.com"},
+    {"id":4, "nome": "Adriana Marciano", "email":"adriana@teste.com"},
+    {"id":5, "nome": "Julia Duarte", "email":"julia@teste.com"},
+    {"id":6, "nome": "Luiz Henrique", "email":"luiz@teste.com"},
+    {"id":7, "nome": "Giovana Maia", "email":"giovana@teste.com"},
+]
